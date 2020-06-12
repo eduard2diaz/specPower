4d97245ef03a594bda9cf95495c28fc5d42d4247
eduardo <eduardo9871@gmx.es>
2020-06-12 14:01:50 -0500

    Matriz de Correlación de Pearson Exportada

--- correlation_matrix.py
+++ correlation_matrix.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
 spec_power_data_file = 'Data/specPowerDataTransform.xlsx'
+correlation_matriz_data_file = 'Data/correlation_matriz.xlsx'
 reader = pd.read_excel(spec_power_data_file, header=0)
 """
 La idea es ir quitando los atributos que estan altamente correlacionados, pues los mismos no aportan
 individualmente informacion en el proceso de clasificacion.
 """
 print("Matriz de correlacion de Pearson")
-print(reader.corr())
+correlation_matrix=reader.corr()
+print(correlation_matrix)
+df=pd.DataFrame(correlation_matrix)
+df.to_excel(correlation_matriz_data_file, index=False)
 #print("Matriz de covarianza")
 #print(reader.cov())
